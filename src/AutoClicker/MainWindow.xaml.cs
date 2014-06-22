2a09f4ef148d1fc7c14113a48bb2b72c2928d02a
Eric Freeman <eric.freeman@extendhealth.com>
2014-06-21 20:49:41 -0600

    Initial commit

---
+++ src/AutoClicker/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace AutoClicker
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            //start clicking
+        }
+    }
+}
