949757cdb3562ed4385573d54e35cd3407d1adb9
Eric Freeman <eric.freeman@extendhealth.com>
2014-06-22 14:58:14 -0600

    You can specify the milliseconds between clicks now.

--- src/AutoClicker/MainWindow.xaml.cs
+++ src/AutoClicker/MainWindow.xaml.cs
@@ -1,20 +1,36 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Forms;
 using System.Windows.Threading;
+using System.Runtime.InteropServices;
 using VirtualInput;
 
 namespace AutoClicker
 {
     public partial class MainWindow
     {
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        [DllImport("user32.dll")]
         public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
 
         public const int MOUSEEVENTF_LEFTDOWN = 0x02;
         public const int MOUSEEVENTF_LEFTUP = 0x04;
 
         private readonly DispatcherTimer _dispatcherTimer = new DispatcherTimer();
+        
+        private int _milliseconds
+        {
+            get
+            {
+                int temp;
+                int.TryParse(MillisecondsEdit.Text, out temp);
+                return temp;
+            }
+            set { MillisecondsEdit.Text = value.ToString(); }
+        }
+
+        #region Constructor
 
         public MainWindow()
         {
@@ -24,17 +40,26 @@ namespace AutoClicker
             VirtualKeyboard.KeyDown += VirtualKeyboardOnKeyDown;
 
             _dispatcherTimer.Tick += dispatcherTimer_Tick;
-            _dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            _milliseconds = 10;
         }
+
+        #endregion
+
+        #region Keyboard Hook
 
         private void VirtualKeyboardOnKeyDown(object sender, KeyEventArgs keyEventArgs)
         {
             if (keyEventArgs.KeyCode == Keys.F2)
             {
+                _dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, _milliseconds);
                 if (!_dispatcherTimer.IsEnabled) _dispatcherTimer.Start();
                 else _dispatcherTimer.Stop();
             }
         }
+
+        #endregion
+
+        #region Click Mouse
 
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
@@ -49,5 +74,7 @@ namespace AutoClicker
             System.Drawing.Point point = Control.MousePosition;
             return new Point(point.X, point.Y);
         }
+
+        #endregion
     }
 }
